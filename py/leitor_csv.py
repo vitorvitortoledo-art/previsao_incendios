0bb0f9f095d57f225b5ab7aaf299c4925ed763c5
Vitor Nogueira de Toledo <vitorvitortoledo@gmail.com>
2026-06-03 22:52:33 -0300

    commit 4

--- py/leitor_csv.py
+++ py/leitor_csv.py
@@ -2,21 +2,18 @@ import pandas as pd
 import os
 
 def ler_csvs_inbe():
-    pasta = r'C:\Users\vitor\OneDrive\Documentos\Github\previsao_incendios\dados'
+    pasta = r'C:\Users\vitor\OneDrive\Documentos\Github\previsao_incendio\dados'
     arquivos = os.listdir(pasta)
     dfs = []
 
     for arquivo in arquivos:
         if arquivo.startswith('focos_br') and arquivo.endswith('.csv'):
             caminho = os.path.join(pasta, arquivo)
-            df = pd.read_csv(caminho, sep=",",  encoding='latin-1')
-            dfs.append(df)
+            
+            # Lê em pedaços de 100 mil linhas
+            for chunk in pd.read_csv(caminho, sep=',', encoding='utf-8', chunksize=100000):
+                dfs.append(chunk)
 
-    df_final = pd.concat(dfs, ignore_index=True)
-    print('Importação feita com sucesso!')
-    return df_final
-
-
-
-ler_csvs_inbe()
-
+    df = pd.concat(dfs, ignore_index=True)
+    print("Arquivos carregados com sucesso!")
+    return df
