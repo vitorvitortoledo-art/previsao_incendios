a34ab90b12b11f79221ac07e3d9263d129fd4453
Vitor Nogueira de Toledo <vitorvitortoledo@gmail.com>
2026-06-03 22:25:18 -0300

    deploy 2

--- py/leitor_csv.py
+++ py/leitor_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def ler_csvs_inbe():
-    pasta = r'C:\Users\vitor\OneDrive\Documentos\Github\previsao_incendio\dados'
+    pasta = r'C:\Users\vitor\OneDrive\Documentos\Github\previsao_incendios\dados'
     arquivos = os.listdir(pasta)
     dfs = []
 
